b854ddf8d422523f4196be9de0e08845faa8f482
tcsondev <tcson.dev@gmail.com>
2022-10-04 18:30:24 +0700

    upcode

--- ForecastMVVMKotlin/app/src/main/java/com/example/forecastmvvmkotlin/ui/MainActivity.kt
+++ ForecastMVVMKotlin/app/src/main/java/com/example/forecastmvvmkotlin/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.forecastmvvmkotlin
+package com.example.forecastmvvmkotlin.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.forecastmvvmkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
